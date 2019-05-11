d172a4f922ab662ea47bedbd56ddb6f42a63ec9e
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-11 20:15:48 +0300

    Update myprofilecompany.php

--- code/myprofilecompany.php
+++ code/myprofilecompany.php
@@ -1,24 +1,33 @@
 <?php
-include("config.php");
-session_start();
-if (!empty($_SESSION))
-{
-    $userID = $_SESSION["userID"];
-    $sql = "SELECT *FROM comp_user NATURAL JOIN general_user WHERE user_ID = '$userID'";
-    $result = mysqli_query($db,$sql);
-    $compUser = mysqli_fetch_object($result);
-    $name = $compUser->company_name;
-    $desc = $compUser->description;
-    $phone= $compUser->phone_no;
-    $pplink =$compUser->pp_link;
-    $email = $compUser->email;
-}
-else
-{
-    header("location: home.php");
-}
+    include("config.php");
+    session_start();
+    if (!empty($_SESSION))
+    {
+        $userID = $_SESSION["userID"];
+        $sql = "SELECT *FROM work_user NATURAL JOIN general_user WHERE user_ID = '$userID'";
+        $result = mysqli_query($db,$sql);
+        $workUser = mysqli_fetch_object($result);
+        $name = $workUser->name;
+        $backInfo = $workUser->background_info;
+        $exp = $workUser->experience;
+        $interests = $workUser->saved_interests;
+        $phone= $workUser->phone_no;
+        $pplink =$workUser->pp_link;
+        $apt_no = $workUser->apartment_no;
+        $street = $workUser->street;
+        $city = $workUser->city;
+        $state = $workUser->state;
+        $country = $workUser->country;
+        $zipcode = $workUser->zipcode;
+        $email = $workUser->email;
+    }
+    else
+    {
+        header("location: home.php");
+    }
 
 ?>
+
 <html>
 <head>
     <link rel="stylesheet" href="style.css">
@@ -29,9 +38,10 @@ else
 <header class="main-header">
     <div class="nav">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a  href="#">Create Job</a></li>
-            <li><a href="#">My Jobs</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="searchjob.php">Job Search</a></li>
+            <li><a href="companysearch.php">Company Search</a></li>
+            <li><a href="myapplications.php">My Applications</a></li>
             <li><a class="active" href="#">My Profile</a></li>
         </ul>
     </div>
@@ -40,73 +50,38 @@ else
 <br>
 <div class="profile_pic">
     <img class="profile_pic" src="<?php echo $pplink; ?>">
-</div>
-<div style="text-align:center">
-    <a href="https://www.w3schools.com/html/">Update Picture</a>
+
 </div>
 <div style="text-align:center">
     <h3><?php echo $name?></h3>
+</div>
+<div style="text-align:center">
+    <a href="https://www.w3schools.com/html/">Update Picture</a>
 </div>
 <br>
 
 <div class="row">
     <div class="column" style="background-color:#aaa;">
 
-        <h2 style="display: inline-block;padding-right: 15px;">Description</h2><a href="https://www.w3schools.com/html/">Edit</a>
-        <p><?php echo $desc?></p>
+        <h2 style="display: inline-block;padding-right: 15px;">Background Information</h2><a href="https://www.w3schools.com/html/">Edit</a>
+        <p><?php echo $backInfo?> </p>
 
-        <h2 style="display: inline-block;padding-right: 31px;">Locations</h2><a  href="https://www.w3schools.com/html/">Edit</a>
-        <p>
-            <?php
-                $locationSql = "SELECT * FROM location WHERE comp_ID = '$userID'";
-                $result2 = mysqli_query($db,$locationSql);
-                    while($location = mysqli_fetch_object($result2)){
-                        $apt_no = $location->apartment_no;
-                        $street = $location->street;
-                        $city = $location->city;
-                        $country = $location->country;
-                        $zipcode = $location->zipcode;
-                        if( $main = $location->mainLocation == 1){
-                            $mainApt = $apt_no;
-                            $mainStreet = $street;
-                            $mainCity = $city;
-                            $mainCountry = $country;
-                            $mainZipcode = $zipcode;
-                            echo("<p> Headquarter $apt_no $street $city $country  $zipcode</p>");
-                        }else {
-                            echo("<p> $apt_no $street $city $country  $zipcode </p>");
-                        }
-                    }
+        <h2 style="display: inline-block;padding-right: 31px;">Experience</h2><a  href="https://www.w3schools.com/html/">Edit</a>
+        <p><?php echo $exp?></p>
 
-            ?>
-        </p>
-
-        <h2>Photos</h2>
-        <div class="row">
-            <div class="column4" style="background-color:#ebb;">
-                <h2>Photo 1</h2>
-            </div>
-            <div class="column4" style="background-color:#bbb;">
-                <h2>Photo 2</h2>
-            </div>
-            <div class="column4" style="background-color:#ccc;">
-                <h2>Photo 3</h2>
-            </div>
-            <div class="column4" style="background-color:#ddd;">
-                <h2>Photo 4</h2>
-            </div>
-        </div>
+        <h2 style="display: inline-block;padding-right: 31px;">Interests</h2><a  href="https://www.w3schools.com/html/">Edit</a>
+        <p><?php echo $interests?></p>
 
     </div>
 
     <div class="column" style="background-color:#bbb;">
         <h2>Contact Information</h2>
         <h3>Email</h3>
-        <p><?php echo $email?></p>
+        <p> <?php echo $email?></p>
         <h3>Phone</h3>
         <p><?php echo $phone?></p>
         <h3>Addres</h3>
-        <p><?php echo $mainApt," ", $mainStreet, " ", $mainCity," ", $mainCountry, " ", $mainZipcode ?> </p>
+        <p><?php echo $apt_no," ", $street, " ", $city," ",$state," ", $country, " ", $zipcode ?> </p>
         <a href="https://www.w3schools.com/html/">Edit</a>
     </div>
 </div>
