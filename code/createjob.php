0cf886b8f28d0570d5a650c857f83f8d14fdd669
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-12 15:32:12 +0300

    nav button geçişleri

--- code/createjob.php
+++ code/createjob.php
@@ -1,37 +1,36 @@
 <?php
-    include('config.php');
-    session_start();
-    $userID = $_SESSION["userID"];
-    $date = date("Y-m-d");
-    echo $date;
-    if(isset($_REQUEST["post"]))
-    {
-    
-        $jobtitle = $_REQUEST["title"];
-        $jobdesc = $_REQUEST["description"];
-        $jobtype = $_REQUEST["type"];
-        $jobloc = $_REQUEST["location"];
-        $jobdept = $_REQUEST["department"];
+include('config.php');
+session_start();
+$userID = $_SESSION["userID"];
+$date = date("Y-m-d");
+echo $date;
+if(isset($_REQUEST["post"]))
+{
 
-        $sql = "INSERT INTO job_offering (date, job_title, job_dept, job_type, office_location,details) VALUES ('$date', '$jobtitle', '$jobdept', '$jobtype', '$jobloc', '$jobdesc')";
-        $result = mysqli_query($db,$sql);
-        
-        $getOFid = "SELECT offering_id FROM job_offering WHERE job_title = '$jobtitle' and job_dept = '$jobdept' and job_type = '$jobtype' and office_location = '$jobloc'";
-        $result2 = mysqli_query($db,$getOFid);
-        $ofID = mysqli_fetch_object($result2);
-        $ofIdFetch = $ofID->offering_id;
-        $sql2 ="INSERT INTO posts (offering_id, user_id) VALUES ('$ofIdFetch', '$userID')";
-        $result3 = mysqli_query($db,$sql2);
-        if($result == 1){
-            header("location: home.php");
-        }
-    }
-    else {
-        $error = "Your Login Name or Password is invalid";
-    }
-    
-    
-   ?>
+$jobtitle = $_REQUEST["title"];
+$jobdesc = $_REQUEST["description"];
+$jobtype = $_REQUEST["type"];
+$jobloc = $_REQUEST["location"];
+$jobdept = $_REQUEST["department"];
+$sql = "INSERT INTO job_offering (date, job_title, job_dept, job_type, office_location,details) VALUES ('$date', '$jobtitle', '$jobdept', '$jobtype', '$jobloc', '$jobdesc')";
+$result = mysqli_query($db,$sql);
+
+$getOFid = "SELECT offering_id FROM job_offering WHERE job_title = '$jobtitle' and job_dept = '$jobdept' and job_type = '$jobtype' and office_location = '$jobloc'";
+$result2 = mysqli_query($db,$getOFid);
+$ofID = mysqli_fetch_object($result2);
+$ofIdFetch = $ofID->offering_id;
+$sql2 ="INSERT INTO posts (offering_id, user_id) VALUES ('$ofIdFetch', '$userID')";
+$result3 = mysqli_query($db,$sql2);
+if($result == 1){
+header("location: home.php");
+}
+}
+else {
+$error = "Your Login Name or Password is invalid";
+}
+
+
+?>
 <html>
 <head>
     <link rel="stylesheet" href="style.css">
@@ -39,23 +38,23 @@
 </head>
 
 <body style="background-color:#FF6F61;">
-    <header class="main-header">
-        <div class="nav">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a class="active" href="#">Create Job</a></li>
-                <li><a href="#">My Jobs</a></li>
-                <li><a href="#">My Profile</a></li>
-            </ul>
-        </div>
-    </header>
+<header class="main-header">
+    <div class="nav">
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a class="active" href="#">Create Job</a></li>
+            <li><a href="myjobscompany.php">My Jobs</a></li>
+            <li><a href="myprofilecompany.php">My Profile</a></li>
+        </ul>
+    </div>
+</header>
+
+<br>
+<h1 style="text-align: center">Create Job</h1>
+
+<form style="text-align:center;">
 
     <br>
-    <h1 style="text-align: center">Create Job</h1>
-
-    <form style="text-align:center;">
-
-        <br>
     <form action="" method="post">
         <label for="title">Job Title</label>
         <input class="input" type="text" placeholder="Enter Job Title" id="title" name ="title" required>
