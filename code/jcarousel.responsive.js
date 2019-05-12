ddb0caecdf1fe78c69b91f82e07a40839decf342
yailke123 <yailke123@gmail.com>
2019-05-12 12:23:24 +0300

    overlay photo opening

--- code/jcarousel.responsive.js
+++ code/jcarousel.responsive.js
@@ -47,3 +47,25 @@
             });
     });
 })(jQuery);
+function newModal(element) {
+    var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+//     var img = document.getElementById(id);
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    // img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = element.src;
+    captionText.innerHTML = element.alt;
+    // }
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+// Get the modal
