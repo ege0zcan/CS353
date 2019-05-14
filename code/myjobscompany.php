e2fa2255064431fb5b7b6182d3ce5d45b387decd
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-14 20:33:16 +0300

    css edit

--- code/myjobscompany.php
+++ code/myjobscompany.php
@@ -12,13 +12,16 @@ else
 }
 
 ?>
+
+/////////////////////////////////////////
 <html>
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>My Jobs</title>
 </head>
 
-<body style="background-color:#FF6F61;">
+<body style="background-color:#dddfd4;">
 <header class="main-header">
     <div class="nav">
         <ul>
@@ -30,27 +33,26 @@ else
     </div>
 </header>
 <br><br><br>
-<body>
 
 <div class="row">
-    <div class="column4" style="background-color:#aba;">
+    <div class="column4">
         <form action="" method="GET">
-            <?php
-                $sql = "SELECT * FROM job_offering NATURAL JOIN posts WHERE user_id = '$userID'";
-                $result = mysqli_query($db,$sql);
-                while($job = mysqli_fetch_object($result)){
-                    $jobID = $job->offering_id;
-                    $jobTitle = $job->job_title;
+        <?php
+        $sql = "SELECT * FROM job_offering NATURAL JOIN posts WHERE user_id = '$userID'";
+        $result = mysqli_query($db,$sql);
+        while($job = mysqli_fetch_object($result)){
+            $jobID = $job->offering_id;
+            $jobTitle = $job->job_title;
 
-                    echo(" <li><button name=\"submit\" type=\"submit\" value=$jobID> $jobTitle </button></li>");
-                }
+            echo(" <li><button name=\"submit\" type=\"submit\" value=$jobID> $jobTitle </button></li>");
+        }
 
         ?>
         </form>
         <div class="pagination">
             <a href="#">&laquo;</a>
-            <a href="#" class="active">1</a>
-            <a href="#" >2</a>
+            <a href="#">1</a>
+            <a href="#" class="active">2</a>
             <a href="#">3</a>
             <a href="#">4</a>
             <a href="#">&raquo;</a>
@@ -58,33 +60,31 @@ else
 
     </div>
 
-    <div class="column" style="background-color:#bbb;">
-        <h1>Applications</h1>
-        <button>Delete Offering</button>
+    <div class="column">
+        <h1 class="create" style="display: inline-block; margin-left: 20px">Applications</h1>
+        <button class="button" style="display: inline-block; height: 50px;margin-left: 475px; font-size: 16px">Delete Offering</button>
         <div class="row">
-            <div class="column4">
-
+            <div class="column4" style="background-color: transparent;">
                 <?php
-                        if(isset($_GET['submit'])) {
-                            $currentID = mysqli_real_escape_string($db, $_GET['submit']);
-                            $sql2 = "SELECT * FROM work_user NATURAL JOIN applies WHERE offering_id ='". $_GET['submit'] ."'";
-                            if($result2 = mysqli_query($db, $sql2)) {
-                                while ($worker = mysqli_fetch_object($result2)) {
-                                    $workID = $worker->user_ID;
-                                    $workName = $worker->name;
-                                    echo("<h1>$workName</h1>");
-                                }
-                            }else{
-                                echo("NO ONE");
-                            }
-                        }else{
-                            echo("here");
+                if(isset($_GET['submit'])) {
+                    $currentID = mysqli_real_escape_string($db, $_GET['submit']);
+                    $sql2 = "SELECT * FROM work_user NATURAL JOIN applies WHERE offering_id ='". $_GET['submit'] ."'";
+                    if($result2 = mysqli_query($db, $sql2)) {
+                        while ($worker = mysqli_fetch_object($result2)) {
+                            $workID = $worker->user_ID;
+                            $workName = $worker->name;
+                            echo("<h1>$workName</h1>");
                         }
+                    }else{
+                        echo("NO ONE");
+                    }
+                }else{
+                    echo("here");
+                }
                 ?>
-
             </div>
-            <div class="column">
-                <h2>Test Answers</h2>
+            <div class="column" style="height: 300px;">
+                <p>Test Answers</p>
             </div>
         </div>
     </div>
