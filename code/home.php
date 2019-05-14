e20011fab01118ff26ca947d13200210d47c6911
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-14 20:31:43 +0300

    css edit

--- code/home.php
+++ code/home.php
@@ -5,19 +5,20 @@
 
 <html>
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Glass Ceiling</title>
 </head>
 
-<body style="background-color:#FF6F61;">
-    <br>
-    <h1 style="text-align:center;" onclick="location.href='http://google.com';" >Glass Ceiling</h1>
-    <br><br><br>
-    <button class="button" onclick="location = 'login.php'";>Login</button>
-    <br><br>
-    <br>
-    <br>
-    <button class="button" onclick="location = 'createaccount.php';" >Create Account</button>
+<body style="background-color:#dddfd4;">
+<br>
+<h1 style="text-align:center;" >GLASS CEILING</h1>
+<br><br><br>
+<button class="button" onclick="location = 'login.php'";>Login</button>
+<br><br>
+<br>
+<br>
+<button class="button" onclick="location = 'createaccount.php';" >Create Account</button>
 </body>
 </html>
 
