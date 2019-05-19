dfbb8e0eb589e7429404f3924bf4d6bdbb6ac6bb
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-20 00:08:54 +0300

    Update searchJob.php

--- code/php/searchJob.php
+++ code/php/searchJob.php
@@ -1,7 +1,21 @@
+<?php
+include("config.php");
+session_start();
+if (!empty($_SESSION))
+{
+    $userID = $_SESSION["userID"];
+
+}
+else
+{
+    header("location: home.php");
+}
+
+?>
 <html>
 <head>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Search Job</title>
 </head>
 
@@ -9,38 +23,63 @@
 <header class="main-header">
     <div class="nav">
         <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="home.php">Home</a></li>
             <li><a class="active" href="#">Job Search</a></li>
-            <li><a href="#">Company Search</a></li>
-            <li><a href="#">My Applications</a></li>
-            <li><a href="#">My Profile</a></li>
+            <li><a href="companysearch.php">Company Search</a></li>
+            <li><a href="myapplications.php">My Applications</a></li>
+            <li><a href="myprofilework.php">My Profile</a></li>
         </ul>
     </div>
 </header>
 <br><br><br>
 
-<form>
-    <input class="input" style="display: inline-block;width: 15%;margin-left: 24.5%" type="text" placeholder="Job Title" name="email" required>
-    <input class="input" style="display: inline-block;width: 15%;margin-left: 20px" type="text" placeholder="Location" name="psw" required>
-    <select class="select" id="type" name="type" style="display:inline-block;width: 15%;margin-left: 20px" required>
-        <option disabled value="">Choose Job Type</option>
+<form style="text-align: center" action="" method="GET">
+    <input class="input" style="display: inline-block;width: 15%;margin-left: 24.5%" type="text" placeholder="Job Title" name="jobTitle">
+    <input class="input" style="display: inline-block;width: 15%;margin-left: 20px" type="text" placeholder="Location" name="location">
+    <select class="select" id="type" name="type" style="display:inline-block;width: 15%;margin-left: 20px">
+        <option value="">Choose Job Type</option>
         <option value="Internship">Internship</option>
         <option value="Full Time">Full Time</option>
         <option value="Part Time">Part Time</option>
     </select>
     <button type="submit" class="button" style="display: inline-block; height: 50px; margin-left: 25px; background-color: transparent; box-shadow:none; color: #173e43; ">Search</button>
 </form>
+<form style="text-align: center" action="" method="POST">
+    <div class="row">
+        <div class="column4">
+<?php
+        if (isset($_GET['jobTitle']) || isset($_GET['location']) || isset($_GET['type'] )){
+            if($_GET['jobTitle']!= "" && $_GET['location']!= "" && $_GET['type']!= "" ) {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE job_title LIKE '".$_GET['jobTitle']."%' & office_location LIKE '".$_GET['location']."%' & job_type LIKE '".$_GET['type']."'% ";
+            }else if($_GET['jobTitle']!= "" && $_GET['location']!= "" ) {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE job_title LIKE '".$_GET['jobTitle']."%' & office_location LIKE '".$_GET['location']."%' ";
+            }else if($_GET['jobTitle']!= ""  && $_GET['type']!= "" ) {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE job_title LIKE '".$_GET['jobTitle']."%'  & job_type LIKE '".$_GET['type']."%' ";
+            }else if( $_GET['location']!= "" && $_GET['type'] != "" ){
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE  & office_location LIKE '".$_GET['location']."%' & job_type LIKE '".$_GET['type']."%' ";
+            }else if( $_GET['jobTitle']!= ""  ) {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE job_title LIKE '".$_GET['jobTitle']."%' ";
+            }else if($_GET['location']!= "") {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE office_location LIKE '".$_GET['location']."%' ";
+            }else if($_GET['type']!= ""  ) {
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE  job_type LIKE '".$_GET['type']."%' ";
+            }else{
+                $sql = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts ";
+            }
 
-<div class="row">
-    <div class="column4">
+            $result = mysqli_query($db, $sql);
+            while($job = mysqli_fetch_object($result)){
 
-        <a href="home.html">Job Title</a><br>
-        <a href="home.html">Job Title</a><br>
-        <a href="home.html">Job Title</a><br>
-        <a href="home.html">Job Title</a><br>
-        <a href="home.html">Job Title</a><br>
-        <a href="home.html">Job Title</a><br>
+                $jobID = $job->offering_id;
+                $jobTitle = $job->job_title;
 
+                echo(" <li><button name=\"submit\" type=\"submit\" value=$jobID> $jobTitle </button></li>");
+            }
+
+        }
+
+    ?>
+    </form>
         <div class="pagination">
             <a href="#">&laquo;</a>
             <a href="#">1</a>
@@ -50,15 +89,33 @@
             <a href="#">&raquo;</a>
         </div>
 
-    </div>
+</div>
 
     <div class="column">
         <h1 class="create" style="display: inline-block; margin-left: 20px">Job Details</h1>
         <button class="button" style="display: inline-block; height: 50px;margin-left: 475px; font-size: 16px">Apply</button>
-        <a style="margin-left: 30px;" href="home.php">Company Profile</a>
+        <a style="margin-left: 30px;" href="home.html">Company Profile</a>
         <div class="row">
             <div class="column1">
-                <p style="display: inline-block; margin-left: 20px;">Bu is cok guzel falan filan falan filan falan filan asd adasd ada das dasd asd ad </p>
+                <?php
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $ID = $_POST["submit"];
+                    $sql2 = "SELECT * FROM job_offering NATURAL JOIN comp_user NATURAL JOIN posts WHERE offering_ID = $ID";
+                    $result2 = mysqli_query($db, $sql2);
+                    if( $offering = mysqli_fetch_object($result2)){
+                        $title = $offering->job_title;
+                        $dept = $offering->job_dept;
+                        $type = $offering->job_type;
+                        $location = $offering->office_location;
+                        $details = $offering->details;
+                        $name = $offering->company_name;
+                        $desc = $offering->description;
+                        echo("<p style=\"display: inline-block; margin-left: 20px;\">JOB TITLE: $title Department: $dept Type: $type Location: $location Details: $details Company Name: $name Description: $desc </p>");
+                    }
+
+
+                }
+                ?>
             </div>
         </div>
     </div>
