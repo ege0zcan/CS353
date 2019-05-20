0d8e1f34d409c180020f59a65040b4853f78b820
Mustafa Bayraktar <31824721+Hagalean@users.noreply.github.com>
2019-05-21 02:05:50 +0300

    Update myjobscompany.php

--- code/php/myjobscompany.php
+++ code/php/myjobscompany.php
@@ -37,22 +37,22 @@ else
 <div class="row">
     <div class="column4">
         <form action="" method="GET">
-        <?php
-        $sql = "SELECT * FROM job_offering NATURAL JOIN posts WHERE user_id = '$userID'";
-        $result = mysqli_query($db,$sql);
-        while($job = mysqli_fetch_object($result)){
-            $jobID = $job->offering_id;
-            $jobTitle = $job->job_title;
+            <?php
+            $sql = "SELECT * FROM job_offering NATURAL JOIN posts WHERE user_id = '$userID'";
+            $result = mysqli_query($db,$sql);
+            while($job = mysqli_fetch_object($result)){
+                $jobID = $job->offering_id;
+                $jobTitle = $job->job_title;
 
-            echo(" <li><button name=\"submit\" type=\"submit\" value=$jobID> $jobTitle </button></li>");
-        }
+                echo(" <li><button name=\"submit\" type=\"submit\" style=\"border: 2px solid #808080; -webkit-transition-duration: 0.4s;padding: 10px 24px; margin:5px; border-radius: 8px;\" value=$jobID> $jobTitle </button></li>");
+            }
 
-        ?>
+            ?>
         </form>
         <div class="pagination">
             <a href="#">&laquo;</a>
-            <a href="#">1</a>
-            <a href="#" class="active">2</a>
+            <a href="#"class="active">1</a>
+            <a href="#">2</a>
             <a href="#">3</a>
             <a href="#">4</a>
             <a href="#">&raquo;</a>
@@ -62,48 +62,49 @@ else
 
     <div class="column">
         <h1 class="create" style="display: inline-block; margin-left: 20px">Applications</h1>
-            <form action="" method="POST">
+        <form action="" method="POST">
 
-                <?php
-                    if(isset($_GET['submit'])) {
-                        echo("<button name=\"test\" type=\"submit\" class=\"button\" style=\"display: inline-block; height: 50px;margin-left: 475px; font-size: 16px\">Add Test</button>");
-                        echo("<button name=\"delete\" type=\"submit\" class=\"button\" style=\"display: inline-block; height: 50px;margin-left: 475px; font-size: 16px\">Delete Offering</button>");
-                        echo("        <div class=\"row\">");
-                        echo("<div class=\"column4\" style=\"background-color: transparent;\">");
-                        $currentID = mysqli_real_escape_string($db, $_GET['submit']);
-                        $_SESSION['idForTest'] = $_GET['submit'];
+            <?php
+            if(isset($_GET['submit'])) {
+                echo("<button name=\"test\" type=\"submit\" class=\"button\" style=\"display: inline-block; width: 20%;height: 5%;margin-left: 30.5px;  font-size: 14px;\">Add Test</button>");
+                echo("      ");
+                echo("<button name=\"delete\" type=\"submit\" class=\"button\" style=\"display: inline-block; width: 20%;height: 5%;margin-left: 30.5px; font-size: 14px;\">Delete Offering</button>");
+                echo("        <div class=\"row\">");
+                echo("<div class=\"column4\" style=\"background-color: transparent;\">");
+                $currentID = mysqli_real_escape_string($db, $_GET['submit']);
+                $_SESSION['idForTest'] = $_GET['submit'];
 
-                        $sql2 = "SELECT * FROM work_user NATURAL JOIN applies WHERE offering_id ='". $_GET['submit'] ."'";
-                        if($result2 = mysqli_query($db, $sql2)) {
-                            while ($worker = mysqli_fetch_object($result2)) {
-                                $workID = $worker->user_ID;
-                                $workName = $worker->name;
-                                $offeringID = $worker->offering_id;
-                                echo(" <li><button name=\"answer\" type=\"submit\" value=$workID> $workName </button></li>");
-                            }
-                        }else{
-                            echo("NO ONE");
-                        }
+                $sql2 = "SELECT * FROM work_user NATURAL JOIN applies WHERE offering_id ='". $_GET['submit'] ."'";
+                if($result2 = mysqli_query($db, $sql2)) {
+                    while ($worker = mysqli_fetch_object($result2)) {
+                        $workID = $worker->user_ID;
+                        $workName = $worker->name;
+                        $offeringID = $worker->offering_id;
+                        echo(" <li><button name=\"answer\" type=\"submit\" style=\"background-color: #e7e7e7;border-radius: 8px; -webkit-transition-duration: 0.4s; \" value=$workID> $workName </button></li>");
                     }
+                }else{
+                    echo("NO ONE");
+                }
+            }
 
-                    if(isset($_POST['test'])){
-                        header("location: createTest.php");
-                    }
-                    if(isset($_POST['delete'])){
-                        $sql3 = "DELETE FROM job_offering WHERE offering_id ='". $_GET['submit'] ."'";
-                        $result3 = mysqli_query($db,$sql3);
-                        header("location: myjobscompany.php");
+            if(isset($_POST['test'])){
+                header("location: createTest.php");
+            }
+            if(isset($_POST['delete'])){
+                $sql3 = "DELETE FROM job_offering WHERE offering_id ='". $_GET['submit'] ."'";
+                $result3 = mysqli_query($db,$sql3);
+                header("location: myjobscompany.php");
 
-                    }
-                    if(isset($_POST['answer'])){
-                        $_SESSION['currentWorkID'] = $_POST['answer'];
-                        header("location: answers.php");
-                    }
-                ?>
-            </div>
-
-        </div>
+            }
+            if(isset($_POST['answer'])){
+                $_SESSION['currentWorkID'] = $_POST['answer'];
+                header("location: answers.php");
+            }
+            ?>
     </div>
+
+</div>
+</div>
 </div>
 
 </body>
